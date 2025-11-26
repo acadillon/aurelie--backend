0f689ec52d1849e10eca3d6f47fe2a57f92c8f2a
Alice Cadillon <acadillon@gmail.com>
2025-11-26 12:19:42 +0100

    creation component slideLandscape et single type sliderLanscape

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,17 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface MediaSlideLandscape extends Struct.ComponentSchema {
+  collectionName: 'components_media_slide_landscapes';
+  info: {
+    displayName: 'SlideLandscape';
+  };
+  attributes: {
+    background: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+    cover: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+    title: Schema.Attribute.String & Schema.Attribute.Private;
+  };
+}
+
 export interface TextGroupField extends Struct.ComponentSchema {
   collectionName: 'components_text_group_fields';
   info: {
@@ -15,6 +27,7 @@ export interface TextGroupField extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'media.slide-landscape': MediaSlideLandscape;
       'text.group-field': TextGroupField;
     }
   }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -662,6 +662,35 @@ export interface ApiMediumMedium extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiSliderLandscapeSliderLandscape
+  extends Struct.SingleTypeSchema {
+  collectionName: 'slider_landscapes';
+  info: {
+    displayName: 'SliderLandscape';
+    pluralName: 'slider-landscapes';
+    singularName: 'slider-landscape';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::slider-landscape.slider-landscape'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    slideLandscape: Schema.Attribute.Component<'media.slide-landscape', true>;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -1176,6 +1205,7 @@ declare module '@strapi/strapi' {
       'api::atlas.atlas': ApiAtlasAtlas;
       'api::exhibition.exhibition': ApiExhibitionExhibition;
       'api::medium.medium': ApiMediumMedium;
+      'api::slider-landscape.slider-landscape': ApiSliderLandscapeSliderLandscape;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
